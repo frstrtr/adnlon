f1906827aede9411f43519b54734c7d84f9f18eb
frstrtr <frstrtr@gmail.com>
2024-11-27 19:36:52 +0400

    initial release

--- go.mod
+++ go.mod
@@ -1,11 +1,17 @@
-module github.com/yourusername/yourproject
+module github.com/frstrtr/adnlon
 
 go 1.22.2
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	go.uber.org/zap v1.27.0
+)
 
 require (
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220328075252-7dd334e3daae // indirect
 	github.com/snksoft/crc v1.1.0 // indirect
 	github.com/tonkeeper/tongo v1.13.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp v0.0.0-20230116083435-1de6713980de // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- main.go
+++ main.go
@@ -1,30 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
 	"github.com/tonkeeper/tongo"
+	"github.com/tonkeeper/tongo/config"
+	"github.com/tonkeeper/tongo/liteapi"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-	// Add your code to use the tongo package here
+func Logger(level string) *zap.Logger {
+	cfg := zap.NewProductionConfig()
 
-	config := tongo.Config{
-		Liteservers: []tongo.Liteserver{
-			{
-				IP:   "127.0.0.1",
-				Port: 4924,
-				Key:  "your-public-key",
-			},
-		},
+	var lvl zapcore.Level
+	if err := lvl.UnmarshalText([]byte(level)); err != nil {
+		panic(err)
 	}
+	cfg.Level.SetLevel(lvl)
 
-	client, err := tongo.NewClient(config)
+	lg, err := cfg.Build()
 	if err != nil {
-		fmt.Println("Error creating client:", err)
-		return
+		panic(err)
 	}
 
-	fmt.Println("Connected to TON liteserver:", client)
+	return lg
+}
+
+func main() {
+	log := Logger("INFO")
+
+	liteServerCFG := config.LiteServer{
+		Host: LiteServerHost,
+		Key:  LiteServerKey,
+	}
+
+	tongoClient, err := liteapi.NewClient(liteapi.WithLiteServers([]config.LiteServer{liteServerCFG}))
+	if err != nil {
+		log.Fatal("NewClient error", zap.Error(err))
+	} else {
+		log.Info("NewClient success")
+	}
+
+	// Parse the account ID
+	accountId := tongo.MustParseAccountID("0:E2D41ED396A9F1BA03839D63C5650FAFC6FCFB574FD03F2E67D6555B61A3ACD9")
+
+	// Get the latest block
+	oneSeqNo, err := tongoClient.GetSeqno(context.Background(), accountId)
+	if err != nil {
+		log.Fatal("Get seqno error", zap.Error(err))
+	}
+
+	// Get the account state
+	state, err := tongoClient.GetAccountState(context.Background(), accountId)
+	if err != nil {
+		log.Fatal("Get account state error", zap.Error(err))
+	}
+
+	// Print the account status and balance
+	fmt.Printf("Account status: %v\nBalance: %v\n", state.Account.Status(), state.Account.Account.Storage.Balance.Grams)
+	// Print the latest block
+	fmt.Printf("Latest block: %v\n", oneSeqNo)
 }
