6e5ab67fc00e2352be430638bfbb12469e66f187
frstrtr <frstrtr@gmail.com>
2024-11-27 16:17:41 +0400

    add .gitignore for go

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tonkeeper/tongo"
 )
 
 func main() {
-	// Example usage of the tongo package
 	fmt.Println("Hello, World!")
 	// Add your code to use the tongo package here
+
+	config := tongo.Config{
+		Liteservers: []tongo.Liteserver{
+			{
+				IP:   "127.0.0.1",
+				Port: 4924,
+				Key:  "your-public-key",
+			},
+		},
+	}
+
+	client, err := tongo.NewClient(config)
+	if err != nil {
+		fmt.Println("Error creating client:", err)
+		return
+	}
+
+	fmt.Println("Connected to TON liteserver:", client)
 }
